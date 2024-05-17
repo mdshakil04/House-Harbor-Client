eb2a30f140a7dcb2681b1adfa5eb615925d89948
mdshakil04 <m.shakil04@yahoo.com>
2024-05-17 16:09:10 +0600

    React Router Installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-4xl text-center'>House Harbor</h1>
+      <button className=' btn btn-outline btn-accent'>Click Me</button>
+    </>
+  )
+}
+
+export default App
