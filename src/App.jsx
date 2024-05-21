ebc8c47fe68e97da0dbcc1912bba41f51e7821b5
mdshakil04 <m.shakil04@yahoo.com>
2024-05-21 19:15:46 +0600

    Daisy Installed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className=' text-4xl text-center'>House Harbor</h1>
-      <button className=' btn btn-outline btn-accent'>Click Me</button>
+      <button className=' btn btn-outline btn-accent'>Daisy Check</button>
     </>
   )
 }
