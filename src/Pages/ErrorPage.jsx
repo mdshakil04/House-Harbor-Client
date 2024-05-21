21d80e65db1169926068bba8986ebaf74d11f6fd
mdshakil04 <m.shakil04@yahoo.com>
2024-05-21 20:40:04 +0600

    Navbar and footer added

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
+        <div className=' flex flex-col justify-center items-center'>
             <h2 className="text-7xl text-center text-red-400 font-bold">404 Page not found</h2>
+            <Link to='/'><button className=' btn  btn-warning'>Go Home</button></Link>
         </div>
     );
 };
