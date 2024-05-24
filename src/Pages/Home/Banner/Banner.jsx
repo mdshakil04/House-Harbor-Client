e4fc9936e02b741603c81486e2e46f783f8734c1
mdshakil04 <m.shakil04@yahoo.com>
2024-05-24 10:37:12 +0600

    Coding Started

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
             Discover a Place You'll Love to Live
           </h1>
           <p className="mb-5">
-          Helping 10 million renters find their perfect fit.
+            Helping 10 million renters find their perfect fit.
           </p>
           <div>
             <label className="input input-bordered flex items-center gap-2">
@@ -23,18 +23,20 @@ const Banner = () => {
                 className="grow text-black "
                 placeholder="Address, City, Zip"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="w-8 h-8  text-black"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="w-8 h-8  text-black"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
             </label>
           </div>
         </div>
