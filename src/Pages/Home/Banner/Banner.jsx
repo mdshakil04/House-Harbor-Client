313560af357e3efd6c47539317e50f472ce4d371
mdshakil04 <m.shakil04@yahoo.com>
2024-06-02 19:55:07 +0600

    Some changes done in UI

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -10,10 +10,10 @@ const Banner = () => {
       <div className="hero-overlay bg-opacity-60 "></div>
       <div className="hero-content text-center text-neutral-content  ">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">
-            Discover a Place You'll Love to Live
+          <h1 className="mb-5 text-5xl text-white font-bold">
+            Discover a Place<br/> You'll Love to Live
           </h1>
-          <p className="mb-5">
+          <p className="mb-5 text-white">
             Helping 10 million renters find their perfect fit.
           </p>
           <div>
