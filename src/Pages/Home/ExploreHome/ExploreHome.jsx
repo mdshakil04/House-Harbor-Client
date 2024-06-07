527babef271bc84e51f755b8432d47563717749a
mdshakil04 <m.shakil04@yahoo.com>
2024-06-07 20:43:27 +0600

    All in today

--- src/Pages/Home/ExploreHome/ExploreHome.jsx
+++ src/Pages/Home/ExploreHome/ExploreHome.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 const ExploreHome = () => {
     return (
-        <div>
+        <div className=" mb-4">
             <h2 className="text-5xl text-center">Explore Home in Dhaka</h2>
-            <p className=' text-center'>Take a deep dive and browse homes for sale,<br/> original neighborhood photos,<br/> resident reviews and local insights to find what is right for you.</p>
+            <p className=' text-center'>Take a deep dive and browse homes for sale, original neighborhood photos,<br/> resident reviews and local insights to find what is right for you.</p>
         </div>
     );
 };
