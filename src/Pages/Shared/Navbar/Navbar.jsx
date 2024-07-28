31f4ba76a366fc3e583d7f9853f86361860abb40
mdshakil04 <m.shakil04@yahoo.com>
2024-07-28 22:57:55 +0600

    Hambarger Updated

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
-
 // import { useState } from "react";
 // import logo from "../../../assets/images/logo/logo.png"
 // import menuIcon from "../../../assets/images/logo/icons8-menu.gif"
 // import Hamburger from "hamburger-react";
-import { Sling as Hamburger } from 'hamburger-react'
-import { Link } from 'react-router-dom';
+// import { Sling as Hamburger } from 'hamburger-react'
+import { Link } from "react-router-dom";
 const Navbar = () => {
   // const [isOpen, setOpen] = useState(false)
   return (
@@ -14,7 +13,21 @@ const Navbar = () => {
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             {/* <img className="h-8 w-8" src={ menuIcon} alt="" /> */}
-            <Hamburger color="#4FD1C5" />
+            {/* <Hamburger color="#4FD1C5" /> */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10 text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
           </div>
           <ul
             tabIndex={0}
@@ -38,13 +51,11 @@ const Navbar = () => {
       </div>
       <div className="navbar-center">
         {/* <img className=" w-12 h-12" src={logo} alt="" /> */}
-        <a className="btn btn-ghost text-xl lg:text-2xl text-white">House Harbor</a>
+        <a className="btn btn-ghost text-xl lg:text-2xl text-white">
+          House Harbor
+        </a>
       </div>
-      <div className="navbar-end">
-      
-
-
-      </div>
+      <div className="navbar-end"></div>
     </div>
   );
 };
